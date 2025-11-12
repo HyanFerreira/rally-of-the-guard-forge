bacf7dd13fbc6bb041dd3d11e8f83003bf19aea5
Hyan Ferreira <hcgamesmg2016@gmail.com>
2025-11-12 14:13:31 -0300

    Initial structure of the Rally of the Guard mod for Forge

---
+++ src/main/java/net/hfstack/rallyguard/RallyOfTheGuard.java
@@ -0,0 +1,14 @@
+package net.hfstack.rallyguard;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RallyOfTheGuard.MOD_ID)
+public final class RallyOfTheGuard {
+    public static final String MOD_ID = "rallyguard";
+
+    public RallyOfTheGuard() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like registries and resources) may still be uninitialized.
+        // Proceed with mild caution.
+    }
+}
